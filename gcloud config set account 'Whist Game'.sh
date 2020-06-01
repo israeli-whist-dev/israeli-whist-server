3d3e9d506c7207711fdb4331999e144e3beea31f
Alon Salzmann <alonzaltsman@gmail.com>
2020-06-01 13:58:03 +0300

    added bash-file

---
+++ gcloud config set account 'Whist Game'.sh
@@ -0,0 +1,3 @@
+gcloud config set account 'Whist Game'
+gcloud container clusters get-credentials whist-game-cluster-production --zone us-central1-c
+gcloud auth activate-service-account --key-file ${HOME}/build/israeli-whist-dev/israeli-whist-server/whist-game-production-c2e47c08e54c.json && kubectl set image deployment/whist-backend whist-backend=hub.docker.com/r/whistteam/whist-game-production/docker-maven-test:$TRAVIS_COMMIT
