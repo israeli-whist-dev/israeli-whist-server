eb1093a0ba1cfa74b53d631a542cc068f55bea08
Alon Salzmann <alonzaltsman@gmail.com>
2020-06-04 18:28:06 +0300

    minor

--- whist_production.sh
+++ whist_production.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 gcloud auth activate-service-account --key-file ${HOME}/build/israeli-whist-dev/israeli-whist-server/whist-game-production-c2e47c08e54c.json
-echo '1'
 gcloud container clusters get-credentials whist-game-cluster-production --zone us-central1-c
-echo '2'
-kubectl set image deployment/whist-backend whist-backend=hub.docker.com/r/whistteam/whist-game-production/docker-maven-test:$TRAVIS_COMMIT
-echo '3'
+
