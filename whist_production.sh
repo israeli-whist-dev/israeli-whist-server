9bff1ef6dccc8c45c6f07e0238a37177c3113026
Alon Salzmann <alonzaltsman@gmail.com>
2020-06-04 19:00:01 +0300

    deleted bash

--- whist_production.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gcloud auth activate-service-account --key-file ${HOME}/build/israeli-whist-dev/israeli-whist-server/whist-game-production-c2e47c08e54c.json
-gcloud container clusters get-credentials whist-game-cluster-production --zone us-central1-c
-
